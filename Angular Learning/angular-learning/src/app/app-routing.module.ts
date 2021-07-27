d497bcdb1d495b28b3c0d769cfba460f6268d364
Udara Vimukthi <uvlakshan@gmail.com>
2021-07-27 20:04:46 +0530

    Added routings for components

--- Angular Learning/angular-learning/src/app/app-routing.module.ts
+++ Angular Learning/angular-learning/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
+const routes: Routes = [
+  { path : '', component: HomeComponent},
+  { path : 'profile', component: ProfileComponent},
+  { path : 'about_us', component: AboutUsComponent}
 
-const routes: Routes = [];
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular Learning/angular-learning/src/app/app.module.ts
+++ Angular Learning/angular-learning/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ProfileComponent
+    ProfileComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
