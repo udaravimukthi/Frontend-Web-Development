d0d44bf53302fdeb8b35e7f8a31dd0461a08ac3f
Udara Vimukthi <uvlakshan@gmail.com>
2021-07-28 02:14:11 +0530

    Added interface and display hardcoded data

--- Angular Learning/angular-learning/src/app/about-us/about-us.component.ts
+++ Angular Learning/angular-learning/src/app/about-us/about-us.component.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Employee{
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+
+}
+
 @Component({
   selector: 'app-about-us',
   templateUrl: './about-us.component.html',
@@ -7,6 +15,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsComponent implements OnInit {
 
+  //making a array for employee interface(hard code)
+  employees: Employee[] = [
+    {id: '1', name: "Chamod", email: 'uv@gmail.com' , password: '123'},
+    {id: '2', name: "Lakshan", email: 'lk@gmail.com' , password: '456'}
+  ];
   constructor() { }
 
   ngOnInit(): void {
