f4f22d0de9ce5e399a06c3123148d70bbf8c043e
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-01 19:29:14 +0530

    Added some changes in files

--- angular-material/src/app/app.module.ts
+++ angular-material/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+// import { MatCheckboxModule} from '@angular/material/checkbox';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    // MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
