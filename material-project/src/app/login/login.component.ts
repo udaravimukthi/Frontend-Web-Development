9e49b09fbcd6c1d401e381c034033084e5223807
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-04 03:56:15 +0530

    Added changes in files

--- material-project/src/app/login/login.component.ts
+++ material-project/src/app/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 
+// export interface Tile {
+//   color: string;
+//   cols: number;
+//   rows: number;
+//   text: string;
+// }
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  // tiles: Tile[] = [
+  //   {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
+  //   {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+  //   {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+  //   {text: 'Four', cols: 2, rows: 2, color: '#DDBDF1'},
+  // ];
   email = new FormControl('', [Validators.required, Validators.email]);
   hide = true;
   constructor() { }
