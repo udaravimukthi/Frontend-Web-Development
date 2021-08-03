82d3abc31df051c12246ce391e1bd8178aefbd86
Udara Vimukthi <uvlakshan@gmail.com>
2021-08-04 03:48:47 +0530

    Added new project changes

--- material-project/src/app/app.module.ts
+++ material-project/src/app/app.module.ts
@@ -12,18 +12,30 @@ import { RegisterComponent } from './register/register.component';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
+import {MatStepperModule} from '@angular/material/stepper';
+import { ProfileComponent } from './profile/profile.component';
+import {MatCardModule} from '@angular/material/card';
+import { TestingComponent } from './testing/testing.component';
+import {  MatGridListModule } from '@angular/material/grid-list'
+import { MatSelectModule} from '@angular/material/select';
+import { Test2Component } from './test2/test2.component'
+import { MatRadioModule } from '@angular/material/radio';
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProfileComponent,
+    TestingComponent,
+    Test2Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule, MatToolbarModule, MatIconModule,
-    MatButtonModule, MatFormFieldModule, MatInputModule ,FormsModule, ReactiveFormsModule
+    MatButtonModule, MatFormFieldModule, MatInputModule ,FormsModule, 
+    ReactiveFormsModule,MatStepperModule, MatCardModule ,MatGridListModule,
+    MatSelectModule, MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
